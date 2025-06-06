5e74eb93b7ce3b622db1829f2fc0b6a5ced2ac40
pmrster <rastpelotty@gmail.com>
2025-06-07 01:11:27 +0700

    feat: prepare for deploy on streamlit cloud

--- app.py
+++ app.py
@@ -33,7 +33,11 @@ if "messages" not in st.session_state:
 with st.sidebar:
     # user_input_api_key = st.text_input("Enter open router API Key",type="password")
     st.write(":gray[this app using open router api]")
-    is_ollama = st.radio(label="select server",options=["open_router", "ollama"])
+    # is_ollama = st.radio(label="select server (local)",options=["open_router", "ollama"]) ##for local test
+    is_ollama = st.radio(label="select server (streamlit cloud)",options=["open_router"]) ## for streamlit cloud
+    st.write(":blue[openrouter will limited to 50 requests]")
+    st.write(":red[_warning: ollama will available only for running on local only!_]")
+    
     if is_ollama == "open_router":
         selected_llm = st.selectbox("select llm model you want to talk with",
                                     ("meta-llama/llama-3.3-8b-instruct:free",
@@ -56,6 +60,7 @@ with st.sidebar:
     elif is_ollama == "ollama":
         selected_llm = st.selectbox("select llm model you want to talk with",
                                     ("qwen3:1.7b",
+                                     "gemma3:4b",
                                      "qwen3:4b",
                                      "qwen3:0.6b",
                                      "qwen3:8b",
@@ -85,11 +90,15 @@ with st.sidebar:
 
 ## chat area
 st.title("chatwithme")
-st.caption("update: 2025.05.29 3:02PM")
+st.caption("update: 2025.06.07 1:09AM")
 st.write(":gray[can using open router api or your local ollama]")
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    if message["role"] == "assistant":
+        avatar = "🐣"
+    elif message["role"] == "user":
+        avatar = "🧑‍💻"
+    with st.chat_message(message["role"],avatar=avatar):
         st.markdown(message["content"])
 
 # Display assistant response in chat message container
@@ -103,6 +112,7 @@ if len(st.session_state.messages) == 0:
 # Accept user input
 if prompt := st.chat_input("What is up?"):
     if prompt == "/quit":
+        ## create file for save session on local device
         file_name = f"{datetime.now().strftime('%Y.%m.%d_%H%M%S')}_session_message_logs.json"
         with open(f"messages_logs/{file_name}", "w", encoding='utf-8') as f:
             json_string = json.dumps(st.session_state.messages, indent=4,ensure_ascii=False)
@@ -111,6 +121,7 @@ if prompt := st.chat_input("What is up?"):
         print(f"saved messages conversation after user say /quit : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
         
         print("end of session")
+        ## reset messages session to blank
         st.session_state.messages = []
         
     
@@ -123,23 +134,31 @@ if prompt := st.chat_input("What is up?"):
         
         # Display assistant response in chat message container
         with st.chat_message("assistant", avatar="🐣"):
-            ## call iilm🤩🥺🐱🐔🐙🐥🐣🧐
+            ## call iilm🤩🥺🐱🐔🐙🐥🐣🧐👻
             # get_instruct_prompt = thai_only_response(user_prompt=response)
             # st.session_state.messages.append({"role": "assistant", "content": get_instruct_prompt})  
             
             new_input_message = [ {"role": m["role"], "content": m["content"]} for m in st.session_state.messages ]
+
             system_prompt  = [{"role": "system","content": thai_only_response()}]
             final_history = system_prompt + new_input_message
             with st.spinner("I'm thinking...", show_time=True):
                 if is_ollama == "open_router":
-                    model_response = AskModel(model=selected_llm, prompt=final_history).ask_with_memory()
-                    response = st.write_stream(StreamResponse(model_response.choices[0].message.content).text_stream_generator())
+                    try:
+                        model_response = AskModel(model=selected_llm, prompt=final_history).ask_with_memory()
+                        response = st.write_stream(StreamResponse(model_response.choices[0].message.content).text_stream_generator())
+                    except Exception as e:
+                        response = f"Error: {e}"
+                        print(f"Error: {e}")
                     st.session_state.messages.append({"role": "assistant", "content": response})
                 elif is_ollama == "ollama":
-                    model_response = AskModel(model=selected_llm, prompt=final_history).ask_with_history_ollama()
-                    # response = StreamResponse(model_response.message.content).write_stream()
-                    response = st.write_stream(StreamResponse(model_response.message.content).text_stream_generator())
-                    
+                    try:
+                        model_response = AskModel(model=selected_llm, prompt=final_history).ask_with_history_ollama()
+                        # response = StreamResponse(model_response.message.content).write_stream()
+                        response = st.write_stream(StreamResponse(model_response.message.content).text_stream_generator())
+                    except Exception as e:
+                        response = f"Error: {e}"
+                        print(f"Error: {e}")
                     st.session_state.messages.append({"role": "assistant", "content": response})
                 
         
